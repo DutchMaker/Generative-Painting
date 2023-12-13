31e0793155c9a5f3f99041024fbf37ca2faa4517
DutchMaker <ruud@partech.nl>
2023-12-13 20:03:39 +0100

    Added Python script for control via GPIO

--- README.md
+++ README.md
@@ -4,3 +4,9 @@
 - Create `.env` file containing `OPENAI_API_KEY="<your_api_key_here>"`
 - Run the server (`node server.js`)
 - Browse to `http://localhost:3000` and get an AI-generated reinterpretation of a famous painting, ready to hang on your wall.
+
+If you want to use the GPIO buttons to reload image or shutdown the Pi:
+
+- Install `xdotool` (`sudo apt-get install xdotool`)
+- Install Python GPIO library (`sudo pip3 install RPi.GPIO`)
+- Add the `buttons.py` script to the crontab to run it at boot
---
+++ buttons.py
@@ -0,0 +1,35 @@
+import RPi.GPIO as GPIO
+import time
+import os
+
+GPIO_RELOAD=2
+GPIO_SHUTDOWN=5
+
+# GPIO setup
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(GPIO_RELOAD, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(GPIO_SHUTDOWN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+# Function to reload Chromium
+def reload_chromium():
+    # Using xdotool to send the F5 key (refresh) to Chromium
+    os.system("export DISPLAY=:0 && xdotool search --onlyvisible --class chromium key F5")
+
+def shutdown():
+    os.system("sudo shutdown now")
+
+# Main loop
+try:
+    while True:
+        # Check if Reload button is pressed
+        if GPIO.input(GPIO_RELOAD) == False:
+            reload_chromium()
+            # Debounce delay
+            time.sleep(0.5)
+        # Check if Shutdown button is pressed
+        if GPIO.input(GPIO_SHUTDOWN) == False:
+            reload_chromium()
+            # Debounce delay
+            time.sleep(0.5)
+except KeyboardInterrupt:
+    GPIO.cleanup()
